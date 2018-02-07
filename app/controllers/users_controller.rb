6aef50dcfce154f986f55cfa1dd45cf93c558a6e
Tommy_Tan <sage.ts920126@gmail.com>
2018-02-08 00:07:33 +0800

    setup user controller tweets & edit & update action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,21 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:tweets, :edit, :update]
   def tweets
+    @user = User.find(params[:id])
+    @tweets = @user.tweets
+    @replies = Reply.new
   end
 
   def edit
+    unless @user == current_user
+      redirect_to users_path(@user)
+    end
   end
 
   def update
+    @user.update(user_params)
+    redirect_to users_path(@user)
   end
 
   def followings
@@ -19,6 +28,16 @@ class UsersController < ApplicationController
 
   def likes
     @likes # 基於測試規格，必須講定變數名稱
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :introduction, :avatar)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 
 end
