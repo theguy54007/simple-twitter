728f11e46b21df39b0e2775e0655ab0382ce0799
Tommy_Tan <sage.ts920126@gmail.com>
2018-02-07 13:09:54 +0800

    add delete action on admin index

--- app/controllers/admin/tweets_controller.rb
+++ app/controllers/admin/tweets_controller.rb
@@ -1,7 +1,24 @@
 class Admin::TweetsController < Admin::BaseController
+  before_action :authenticate_user!
+  before_action :authenticate_admin
+
   def index
+    @tweets = Tweet.page(params[:page]).per(10)
+    @users = User.all
   end
 
   def destroy
+    @tweet = Tweet.find(params[:id])
+    @tweet.destroy
+    redirect_to admin_tweets_path
+    flash[:alert] = "Tweet was deleted"
   end
+
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:description)
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,18 @@ Rails.application.routes.draw do
   devise_for :users
 
   # 請依照專案指定規格來設定路由
+  resources :tweets, only: [:index, :create] do
+    resources :replies, only: [:index, :create, :destroy]
+  end
+
   root "tweets#index"
 
   namespace :admin do
+    resources :tweets, only: [:index, :destroy]
     root "tweets#index"
   end
+
+
+  resources :users, only: [:index, :show, :edit, :update]
+
 end
