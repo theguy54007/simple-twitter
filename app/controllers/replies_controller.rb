32d9338c6cc113d08532679c37556fff646058b6
Tommy_Tan <sage.ts920126@gmail.com>
2018-02-07 23:44:54 +0800

    setup replies controller index & create action

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -1,9 +1,29 @@
 class RepliesController < ApplicationController
 
+  before_action :set_reply, only:[:index, :create]
   def index
+    @tweet = Tweet.find(params[:tweet_id])
+    @replies = Reply.new
+    @reply = @tweet.replies
   end
 
   def create
+    @reply = @tweet.replies.build(reply_params)
+    @reply.user = current_user
+    @reply.save!
+    redirect_to tweet_path(@tweet)
+  end
+
+  private
+
+  def reply_params
+    params.require(:reply).permit(:comment)
+  end
+
+
+  def set_reply
+    @tweet = Tweet.find(params[:tweet_id])
+    @reply = @tweet.replies
   end
 
 end
