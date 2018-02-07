45569ce4420095732b2ee127fde4e2e0bc8487db
Tommy_Tan <sage.ts920126@gmail.com>
2018-02-07 13:09:05 +0800

    add tweet in tweeter index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,10 +1,18 @@
 class TweetsController < ApplicationController
 
+  before_action :authenticate_user!
+
   def index
-    @users # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @users =User.all # 基於測試規格，必須講定變數名稱，請用此變數中存放關注人數 Top 10 的使用者資料
+    @tweet = Tweet.new
+    @tweets = Tweet.all
   end
 
   def create
+    @tweet = Tweet.new(tweet_params)
+    @tweet.user = current_user
+    @tweet.save!
+    redirect_to root_path
   end
 
   def like
@@ -12,5 +20,12 @@ class TweetsController < ApplicationController
 
   def unlike
   end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:description)
+
+  end
+
 
 end
