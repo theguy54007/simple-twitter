01772b623beb3b37540e7a593784953ee8b81609
Tommy_Tan <sage.ts920126@gmail.com>
2018-02-08 12:36:10 +0800

    correct belong_to  -> belongs_to

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,5 +1,5 @@
 class Followship < ApplicationRecord
   validates :following_id, uniqueness: { scope: :user_id }
-  belong_to :user
-  belong_to :following, class_name: "User"
+  belongs_to :user
+  belongs_to :following, class_name: "User"
 end
