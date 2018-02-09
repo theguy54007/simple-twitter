e27ea61f995e61897c15b83c1c78500d681f97df
Tommy_Tan <sage.ts920126@gmail.com>
2018-02-09 10:56:16 +0800

    setup route for followings and followships

--- config/routes.rb
+++ config/routes.rb
@@ -25,6 +25,9 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show, :edit, :update] do
     member do
       get :tweets
+      get :followings
+      get :followers
+      
     end
   end
 
