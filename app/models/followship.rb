e549e4a2f8036f11986559ab7cabad86e036e4ad
Tommy_Tan <sage.ts920126@gmail.com>
2018-02-21 01:35:01 +0800

    make correction

--- app/models/followship.rb
+++ app/models/followship.rb
@@ -1,6 +1,6 @@
 class Followship < ApplicationRecord
   validates :following_id, uniqueness: { scope: :user_id }
 
-  belongs_to :following, class_name: "User"
-  belongs_to :user, counter_cache: :followers_count
+  belongs_to :following, class_name: "User", counter_cache: :followers_count
+  belongs_to :user
 end
